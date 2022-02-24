52779a0054f47756d2c46a443e5ea37233be6f80
Ashpreet Kaur <75926645+ashpreet12@users.noreply.github.com>
2022-02-24 15:10:21 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (5.76%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void printMap(unordered_map<int,int> m){
+        for(auto x : m){
+            cout<<x.first<<" "<<x.second<<"   ";
+        }
+        cout<<endl;
+    }
+    unordered_map<int,int> createMap(vector<int> arr){
+        unordered_map<int,int> m;
+        for(auto num : arr){
+            m[num]++;
+        }
+        return m;
+    }
+    void putValues(vector<int> &ans,int val,int times){
+        for(int i = 0; i < times;i++){
+            ans.push_back(val);
+        }
+    }
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> m1 = createMap(nums1);
+        unordered_map<int,int> m2 = createMap(nums2);
+        
+        vector<int> ans;
+       
+        for(auto &x : m1){
+            auto y = m2.find(x.first);
+            if(y != m2.end()){
+                putValues(ans,x.first,min(x.second, y->second));
+            }
+        }
+        return ans;
+    }
+};
