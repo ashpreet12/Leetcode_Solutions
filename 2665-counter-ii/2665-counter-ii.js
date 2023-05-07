07b5ebeff940cce934b563aaa41acdba6ee1b87e
Ashpreet Kaur <75926645+ashpreet12@users.noreply.github.com>
2023-05-07 11:15:59 +0530

    Time: 63 ms (75.07%), Space: 44.7 MB (54.74%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let current = init;
+    var obj = {
+        increment : function(){
+            current++;
+            return current;
+        },
+        reset : function(){
+            current = init;
+            return current;
+        },
+        decrement : function(){
+            current--;
+            return current;
+        }
+    }
+    return obj;
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
